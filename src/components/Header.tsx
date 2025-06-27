b9010bb68e570064e387ee729177b606e1bebe54
james <omotoyosiadebeshin@gmail.com>
2025-06-27 23:40:25 +0100

    state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="px-4 pt-4 pb-4 border-b-1 border-gray-700 fixed right-0 left-0">
       <nav className="flex justify-between items-center mx-auto">
-        <div>
+        <Link to="/">
           <img className="h-7" src="./rivo.png" />
-        </div>
-        <div className="h-8 w-8 text-gray-400" title="New chat">
+        </Link>
+        <div className="h-8 w-8 text-gray-400 cursor-pointer" title="New chat">
           <ChatBubbleOvalLeftIcon />
         </div>
       </nav>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,28 @@
 import { ArrowUpIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 export default function Input() {
+  const [message, setMessage] = useState("");
+
   return (
     <div className="relative w-full">
       <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
         placeholder="Message Rivo"
         className="pl-4 pr-12 py-3 rounded-lg placeholder:text-gray-400 w-full bg-[#404045] outline-none text-white focus:ring-2 focus:ring-blue-500 transition-all"
       />
       <button
         type="submit"
-        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full p-2 bg-gray-400 hover:bg-gray-300 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500"
+        disabled={message.length < 3}
+        className={`${
+          message.length < 3
+            ? "cursor-not-allowed bg-gray-400 text-[#404045]"
+            : "cursor-pointer bg-blue-500 text-white"
+        } absolute right-2 top-1/2 -translate-y-1/2 rounded-full p-2 hover:bg-gray-300 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500`}
         aria-label="Send message"
       >
-        <ArrowUpIcon className="h-5 w-5 text-[#404045]" />
+        <ArrowUpIcon className="h-5 w-5" />
       </button>
     </div>
   );
