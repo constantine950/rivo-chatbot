bec42be5844714112f8eed596443ec8be5688a42
james <omotoyosiadebeshin@gmail.com>
2025-07-07 19:13:26 +0100

    notfound

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -2,7 +2,7 @@ import Input from "../components/Input";
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center text-center">
+    <div className="flex h-full flex-col items-center justify-center text-center">
       <div className="mx-auto w-full max-w-md px-4">
         <div className="text-white mb-6">
           <h1 className="text-2xl font-medium">Hi, I'm Rivo</h1>
--- src/page/NotFoundPage.tsx
+++ src/page/NotFoundPage.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="flex flex-col h-[calc(100vh-73px)] mt-[73px] items-center justify-center text-center bg-[#292A2D] text-white">
+      <div className="max-w-4xl mx-auto px-4">
+        <h1 className="text-5xl font-bold mb-6">404</h1>
+        <h2 className="text-2xl font-medium mb-4">Page Not Found</h2>
+        <p className="text-gray-300 mb-8">
+          The page you're looking for doesn't exist
+        </p>
+        <Link
+          to="/"
+          className="inline-block px-6 py-3 bg-blue-500 hover:bg-blue-600 rounded-lg text-white font-medium transition-colors"
+        >
+          Return Home
+        </Link>
+      </div>
+    </div>
+  );
 }
